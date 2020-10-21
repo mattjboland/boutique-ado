b209370c0b52d8b4e96c92e7593afdd05957a5bc
Matt Boland <mattjamesboland@gmail.com>
2020-10-21 19:34:35 +0000

    created check out app and models

--- checkout/models.py
+++ checkout/models.py
@@ -51,6 +51,74 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+# With those imports done, let's write a few quick model methods.
+# In the order model, I'll begin with a method called generate order number.
+# And it's prepended with an underscore by convention to indicate it's a
+# private method which will only be used inside this class.
+# All it's going to do is return uuid.uuid4().hex.upper()
+# which will just generate a random string of 32 characters we can use
+# as an order number.
+
+
+def _generate_order_number(self):
+    """
+    Generate a random, unique order number using UUID
+    """
+    return uuid.uuid4().hex.upper()
+
+# Also in this model let's write a method to update the total which we
+# can do using the aggregate function.
+# The way this works is by using the sum function across all the
+# line-item total fields for all line items on this order.
+# The default behaviour is to add a new field to the query set
+# called line-item total sum. Which we can then get and set the order
+# total to that. With the order total calculated, we can then
+# calculate the delivery cost using the free delivery threshold
+# and the standard delivery percentage from our settings file.
+# Setting it to zero if the order total is higher than the threshold.
+# And then to calculate the grand total
+# all we have to do is add the order total and the delivery cost
+# together and save the instance.
+
+
+def update_total(self):
+    """
+    Update grand total each time a line item is added,
+    accounting for delivery costs.
+    """
+    self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+    if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+    else:
+        self.delivery_cost = 0
+    self.grand_total = self.order_total + self.delivery_cost
+    self.save()
+
+# Next I'll override the default save method.
+# So that if the order we're saving right now doesn't
+# have an order number.
+# We'll call the generate order number method.
+# And then execute the original save method.
+
+
+def save(self, *args, **kwargs):
+    """
+    Override the original save method to set the order number
+    if it hasn't been set already.
+    """
+    if not self.order_number:
+        self.order_number = self._generate_order_number()
+    super().save(*args, **kwargs)
+
+# Finally for both of these models.
+# I'll add the standard string method,
+# returning just the order number for the order model.
+
+
+def __str__(self):
+    return self.order_number
+
+
 # Whenever an order is saved.
 # One last key thing in this model is the editable equals
 # false attribute on the order number field.
@@ -91,3 +159,27 @@ class OrderLineItem(models.Model):
     product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+# Like setting the order number on the order model we
+# also need to set the line-item total field
+# on the order line-item model by overriding its save method.
+# The logic is simple though, we just need to multiply the
+# product price by the quantity for each line item.
+
+
+def save(self, *args, **kwargs):
+    """
+    Override the original save method to set the lineitem total
+    and update the order total.
+    """
+    self.lineitem_total = self.product.price * self.quantity
+    super().save(*args, **kwargs)
+
+
+# Finally for both of these models.
+# And the SKU of the product along with the order
+# number it's part of for each order line item.
+
+
+def __str__(self):
+    return f'SKU {self.product.sku} on order {self.order.order_number}'
