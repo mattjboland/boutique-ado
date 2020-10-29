20867dfa8ab6c806a47c5140e7fb58fdaf9fb96f
Matt Boland <mattjamesboland@gmail.com>
2020-10-29 18:41:16 +0000

    Completed webhook handler

--- checkout/models.py
+++ checkout/models.py
@@ -50,6 +50,8 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_bag = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
 # With those imports done, let's write a few quick model methods.
 # In the order model, I'll begin with a method called generate order number.
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -39,6 +39,12 @@
 
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
+
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -80,9 +86,143 @@ class StripeWH_Handler:
         # our metadata attached. The payment intent will be saved in a key
         # called event.data.object. So we'll store that and print it out.
         intent = event.data.object
-        print(intent)
+        """
+        In the payment intent succeeded webhook handler we've already got the
+        payment intent which has all our customers information in it. All we
+        need to do is use it to create an order just like we did with the form.
+        The only reason we're doing this is in case the form isn't submitted
+        for some reason like if the user closes the page on the loading screen.
+        To get started let's get the payment intent id, as well as the
+        shopping bag and the users save info preference from the metadata we
+        added in the last video. I'll also store the billing details, shipping
+        details, and grand_total. All of which we'll need in a moment.
+        Now to ensure the data is in the same form as what we want in our
+        database. I'll replace any empty strings in the shipping details with
+        none. Since stripe will store them as blank strings which is not the
+        same as the null value we want in the database. Now that the data is
+        clean and ready to go let's talk about the basic process. Most of the
+        time when a user checks out, everything will go well and the form will
+        be submitted so the order should already be in our database when we
+        receive this webhook.
+        The first thing then is to check if the order exists already.
+        If it does we'll just return a response, and say everything is all set.
+        And if it doesn't we'll create it here in the webhook.
+        Let's start by assuming the order doesn't exist.
+        We can do that with a simple variable set to false.
+        Then we'll try to get the order using all the information from the
+        payment intent. And I'm using the iexact lookup field to make it an
+        exact match but case-insensitive. If the order is found we'll set
+        order exists to true, and return a 200 HTTP response to stripe, with
+        the message that we verified the order already exists.
+        If it doesn't exist let's create it just like we would if the form
+        were submitted. In fact, we can get almost all the code we need from
+        the view that does the same thing. We'll still want to iterate through
+        the bag items, the only difference here is that we're going to load the
+        bag from the JSON version in the payment intent
+        instead of from the session.
+        Also, we don't have a form to save in this webhook to create the order
+        but we can do it just as easily with order.objects.create
+        using all the data from the payment intent.
+        After all, it came from the form originally anyway
+        I'll wrap this whole thing in a try block.
+        And if anything goes wrong I'll just delete the order if it was
+        created. And return a 500 server error response to stripe.
+        This will cause stripe to automatically try the webhook again later.
+        """
+        pid = intent.id
+        bag = intent.metadata.bag
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # Clean data in the shipping details
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        # Instead of just immediately going to create the order if it's
+        # not found in the database. let's introduce a bit of delay.
+        # I'll create a simple variable called attempt and set it to 1.
+        # Now let's create a while loop that will execute up to 5 times.
+        # I'll move all this code into the loop, but now instead of creating
+        # the order if it's not found the first time. I'll increment attempt
+        # by 1. And then use pythons time module to sleep for one second.
+        # This will in effect cause the webhook handler to try to find the
+        # order five times over five seconds before giving up and creating the
+        # order itself. Now since the attempt is in a while loop though, if
+        # the order is found we should break out of the loop. attempt = 1
+
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                for item_id, item_data in json.loads(bag).items():
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
